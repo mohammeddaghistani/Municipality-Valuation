1aacc411fb8f7ce237099d2e1417ba19d348ef92
mohammeddaghistani <mdsa2003@gmail.com>
2026-01-17 02:53:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.express as px
+
 from core_valuation import (
     residual_value,
     estimated_rent,
@@ -732,4 +732,5 @@ with tab4:
         st.write(f"- درجة الثقة: **{conf.get('text','')}**")
 
     st.markdown("</div>", unsafe_allow_html=True)
+
 
